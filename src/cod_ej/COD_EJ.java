779fafb9d939f3e2a7da8f5ba0b1bdadfc5ee62f
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2017-11-03 09:26:16 +0100

    Inicio Proyecto

---
+++ src/cod_ej/COD_EJ.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cod_ej;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ldizbarros
+ */
+public class COD_EJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduzca o nome da primeira persoa:");
+        String nome1 = sc.next();
+        System.out.println("Introduzca o peso primeira persoa:");
+        float peso1 = sc.nextFloat();
+        System.out.println("Introduzca o nome da segunda persoa:");
+        String nome2 = sc.next();
+        System.out.println("Introduzca o peso da segunda persoa:");
+        float peso2 = sc.nextFloat();
+        
+        if (peso1>peso2){
+            System.out.println("Nome: "+nome1+" Peso: "+peso1);
+            System.out.println("DIferencia: "+(peso1-peso2));
+        }
+        else{
+            System.out.println("Nome: "+nome2+" Peso: "+peso2);
+            System.out.println("DIferencia: "+(peso2-peso1));
+        }
+    }
+    
+}
